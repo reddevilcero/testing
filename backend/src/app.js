fa4e59716bfc48afe9a6deba9f3af978609d6a6f
Ignacio Perez <ignacioperez@Ignacios-MBP.broadband>
2020-05-09 20:04:34 +0100

    merging

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-<<<<<<< HEAD
 app.use(function (req, res, next) {
-=======
-app.use(function(req, res, next) {
->>>>>>> 1d8b6c7add4c1e2ac991fe971bc73828dc34be71
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
@@ -23,10 +19,7 @@ app.use(express.json());
 
 //routes
 app.use("/api/users", require("./routes/users"));
-<<<<<<< HEAD
 app.use("/api/useraddress", require("./routes/userAddress"));
-=======
->>>>>>> 1d8b6c7add4c1e2ac991fe971bc73828dc34be71
 app.use("/api/bookings", require("./routes/bookings"));
 app.use("/api/auth", require("./routes/authentication"));
 
--- backend/src/middlewares/authenticationMiddleware.js
+++ backend/src/middlewares/authenticationMiddleware.js
@@ -3,22 +3,14 @@ const authMiddlewares = {};
 const jwt = require("jsonwebtoken");
 
 authMiddlewares.IsAuthenticated = (req, res, next) => {
-<<<<<<< HEAD
   const authHeader = req.headers["authorization"];
-=======
-  /*const authHeader = req.headers["authorization"];
->>>>>>> 1d8b6c7add4c1e2ac991fe971bc73828dc34be71
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
     next();
-<<<<<<< HEAD
   });
-=======
-  });*/
->>>>>>> 1d8b6c7add4c1e2ac991fe971bc73828dc34be71
   next();
 };
 
